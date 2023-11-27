452594e1dc71ec83eec891b0a39e5e462df8e102
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-11-27 15:46:58 +0200

    Add half-working remove_by

--- menu.rb
+++ menu.rb
@@ -6,6 +6,7 @@ class MenuStorage
     @storage = []
     @is_submenu = false
     @is_remove_mode = false
+    @is_remove_by_index = false
   end
 
   def add(function_to_invoke)
@@ -42,6 +43,34 @@ class MenuStorage
     end
   end
 
+  def remove_by(name)
+    item_index = get_item_index_by_name(name)
+
+    if item_index == nil
+      return false
+    end
+
+    remove_at(item_index)
+  end
+
+  def get_item_index_by_name(name)
+    @storage.each_with_index do |item, index|
+      if item.is_a? Symbol
+        if item.id2name == name
+          return index
+        end
+      end
+
+      if item.is_a Hash
+        if item[:name] == name
+          return index
+        end
+      end
+    end
+
+    nil
+  end
+
   def invoke(index)
     item = @storage[index]
 
@@ -68,11 +97,19 @@ class MenuStorage
       printf "> "
       user_choice = gets.strip
 
-      is_to_enable_remove_mode = user_choice.downcase == 'x' if !@is_remove_mode
+      is_to_enable_remove_mode = user_choice.downcase == 'x' ||
+        user_choice.downcase == 'y' if !@is_remove_mode
 
       if is_to_enable_remove_mode
         puts "Enter remove mode"
         @is_remove_mode = true
+
+        if user_choice.downcase == 'x'
+          @is_remove_by_index = true
+        else
+          @is_remove_by_index = false
+        end
+
         next
       end
 
@@ -80,18 +117,27 @@ class MenuStorage
       is_shortcut_correct = !item_shortcut.zero?
 
       unless is_shortcut_correct
-        if @is_remove_mode
-          puts "Leave remove mode"
-          @is_remove_mode = false
-          next
-        end
+        # TODO: Add proper logic for removing (@biggujo)
+        # is_in_remove_by_index = @is_remove_mode && @is_remove_by_index
+        # is_in_remove_by_position = @is_remove_mode && !@is_remove_by_index
+        # choice_is_zero = user_choice == "0"
+        #
+        # if @is_remove_mode || (is_in_remove_by_position && choice_is_zero)
+        #   puts "Leave remove mode"
+        #   @is_remove_mode = false
+        #   next
+        # end
 
         # Exit on 0 given (0 - Return)
         return
       end
 
       if @is_remove_mode
-        next if remove_at(item_shortcut - 1)
+        if @is_remove_by_index
+          next if remove_at(item_shortcut - 1)
+        end
+
+        next if remove_by(user_choice)
       else
         next if invoke(item_shortcut - 1)
       end
@@ -109,7 +155,8 @@ class MenuStorage
       end
     end
 
-    puts "X - Remove" unless @is_remove_mode
+    puts "X - Remove by shortcut" unless @is_remove_mode
+    puts "Y - Remove by name" unless @is_remove_mode
 
     if @is_remove_mode
       puts "0 - Exit remove mode"
